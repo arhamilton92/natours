fbcd43dbef7606e67f1a2778ca75e23a53cbcd2d
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-02 14:12:40 -0400

    removing node_modules

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const express = require('express');
 const morgan = require('morgan');
 
 // DEFINE ROUTERS
-const tourRouter = require('./routes/tourRoutes.js')
-const userRouter = require('./routes/userRoutes.js')
+const tourRouter = require('./routes/tourRoutes.js');
+const userRouter = require('./routes/userRoutes.js');
 
 // DEFINE EXPRESS
 const app = express();
 
 // MIDDLWARE
+if (process.env.NODE_ENV === 'development') {
+    console.log('development');
+    app.use(morgan('dev'));
+}
+
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 /// USE ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-module.exports = app
+module.exports = app;
