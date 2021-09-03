e88ad4e833db381df8bc60743e0055b4f906a35e
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-03 15:57:31 -0400

    moved process.exit outside of try/catch blocks

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -26,20 +26,20 @@ const importData = async () => {
     try {
         await Tour.create(tours);
         console.log('data loaded')
-        process.exit()
     } catch (error) {
         console.log(error)
     }
+    process.exit()
 };
 
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
         console.log('data deleted')
-        process.exit()
     } catch (error) {
         console.log(error)
     }
+    process.exit()
 }
 
 if(process.argv[2] === '--import') {
