ce8d1c922b6d0c53898abb3c68083a1a84285043
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-03 13:22:11 -0400

    created tour schema & model

--- server.js
+++ server.js
@@ -4,17 +4,41 @@ const app = require('./app'); // EXPRESS
 
 dotenv.config({ path: './.env' }); // ENV variables
 
-// DATABASE //
+// DATABASE 
 const password = process.env.DATABASE_PASSWORD;
 const DB = process.env.DATABASE.replace('<PASSWORD>', password);
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(console.log('DB connection successful!'));
+// ------------------------------------
+
+// SCHEMA 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
 }); // --------------------------------
 
-// SERVER //
+// MODEL 
+const Tour = mongoose.model('Tour', tourSchema);
+// ------------------------------------
+
+// SERVER 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
