c5fabd7ae369105a7ac99f36b444c73ad29bc068
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-03 13:15:10 -0400

    setting up DB connection, reformatting

--- app.js
+++ app.js
@@ -3,24 +3,23 @@
 const express = require('express');
 const morgan = require('morgan');
 
-// DEFINE ROUTERS
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-
-// DEFINE EXPRESS
 const app = express();
 
-// MIDDLWARE
+// MIDDLEWARE //
 if (process.env.NODE_ENV === 'development') {
     console.log('development');
     app.use(morgan('dev'));
 }
-
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+// ------------------------------------
 
-/// USE ROUTES
+// ROUTER //
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+// ------------------------------------
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-/** @format */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app'); // EXPRESS
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: './.env' }); // ENV variables
 
-const app = require('./app');
+// DATABASE //
+const password = process.env.DATABASE_PASSWORD;
+const DB = process.env.DATABASE.replace('<PASSWORD>', password);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+}); // --------------------------------
 
+// SERVER //
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
-});
-
+}); // --------------------------------
