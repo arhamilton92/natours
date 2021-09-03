ca5e12df03b52c5cf14db8a942e994d8e1167a69
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-03 16:54:56 -0400

    added query filtering to getAllTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,8 +4,21 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
 	try {
-		const tours = await Tour.find();
+		// BUILD QUERY
+		// 1: filtering
+		const queryObj = { ...req.query };
+		const excludedFields = ['page', 'sort', 'limit', 'fields'];
+		excludedFields.forEach((el) => delete queryObj[el]);
 		//
+
+		// 2: advanced filtering
+		let queryStr = JSON.stringify(queryObj)
+		queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+
+		const query = Tour.find(JSON.parse(queryStr));
+		//
+		// EXECUTE QUERY
+		const tours = await query
 		res.status(200).json(tours);
 	} catch (error) {
 		res.status(404).json({
@@ -67,13 +80,13 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
 	try {
-		await Tour.findByIdAndDelete(req.params.id)
+		await Tour.findByIdAndDelete(req.params.id);
 		//
 		res.status(204).json({
 			status: 'success',
 			message: 'tour deleted',
-			data: null
-		})
+			data: null,
+		});
 	} catch (error) {
 		res.status(400).json({
 			status: 'failed',
