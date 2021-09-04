3e395a13152c68ec19c77c77271b5f66c8d28bf5
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-04 14:03:23 -0400

    added: app.all // incorrect URL error handdler

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 
 const app = express();
 
-// MIDDLEWARE //
+// MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
 	console.log('development');
 	app.use(morgan('dev'));
@@ -14,12 +14,18 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 // ------------------------------------
 
-// ROUTER //
+// ROUTER
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
+//
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-// ------------------------------------
+// incorrect URL error handler
+app.all('*', (req, res, next) => {
+	res.status(404).json({
+		status: 'fail',
+		message: `Can't find ${req.originalUrl}`,
+	});
+}); // --------------------------------
 
 module.exports = app;
