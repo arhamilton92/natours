3992ece005d970fcc5dd01bf2ccff6048e9c8fa3
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-04 16:14:40 -0400

    added: sendErrorDev & sendErrorProd // more detailed error if in developement state, less detailed production.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,29 @@
-module.exports = (err, req, res, next) => {
-	err.statusCode = err.statusCode || 500;
-	err.status = err.status || 'error';
-	//
+/** @format */
+
+const sendErrorDev = (err, res) => {
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message,
+		error: err,
+		stack: err.stack,
+	});
+};
+
+const sendErrorProd = (err, res) => {
 	res.status(err.statusCode).json({
 		status: err.status,
 		message: err.message,
 	});
-}
+};
+
+module.exports = (err, req, res, next) => {
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || 'error';
+	//
+
+	if (process.env.NODE_ENV === 'development') {
+		sendErrorDev(err, res);
+	} else if (process.env.NODE_ENV === 'production') {
+		sendErrorProd(err, res);
+	}
+};
