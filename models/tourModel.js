4d445b608b28dc481c24dfc4234e0b1ceba48ceb
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-04 11:42:00 -0400

    updated: tourSchema.pre(find) // now runs pre any query starting with find

--- models/tourModel.js
+++ models/tourModel.js
@@ -82,7 +82,7 @@ tourSchema.pre('save', function (next) {
 }); // --------------------------------
 
 // QUERY MIDDLEWARE
-tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
 	this.find({ secretTour: false });
 	next();
 }); // --------------------------------
