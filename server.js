8e19cee2b29c872863e31596bdfb4403bccb41c9
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-04 19:23:57 -0400

    reformatting

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = require('./app'); // EXPRESS
 process.on('uncaughtException', (err) => {
 	console.log('uncaught exception');
 	console.log(err.name, err.message);
-	process.exit()
+	process.exit();
 }); // --------------------------------
 
 // DATABASE
@@ -32,9 +32,10 @@ const server = app.listen(port, () => {
 	console.log(`App running on port ${port}`);
 }); // --------------------------------
 
+// UNHANDLED REJECTION HANDLER
 process.on('unhandledRejection', (err) => {
 	console.log(err.name, err.message);
 	server.close(() => {
 		process.exit(1);
 	});
-});
+}); // --------------------------------
