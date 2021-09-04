63e5a940ed3ded38c8bb733c39eabd08d5176a0a
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-04 14:21:41 -0400

    added & updated: app.use(error handler) // created general error handler. updated app.all() url error middleware into new error handler.

--- app.js
+++ app.js
@@ -20,11 +20,22 @@ const userRouter = require('./routes/userRoutes');
 //
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-// incorrect URL error handler
+// incorrect URL error
 app.all('*', (req, res, next) => {
-	res.status(404).json({
-		status: 'failed',
-		message: `Can't find ${req.originalUrl}`,
+	const err = new Error(`Can't find ${req.originalUrl}`)
+	err.status = 'failed';
+	err.statusCode = 404
+	//
+	next(err)
+});
+// error handling
+app.use((err, req, res, next) => {
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || 'error';
+	//
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message,
 	});
 }); // --------------------------------
 
