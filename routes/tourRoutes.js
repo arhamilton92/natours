771f476e9562b027e10d2a05aee3807ffd74b4df
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-03 21:17:36 -0400

    getTourStats aggregation function

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,14 +8,17 @@ const {
 	getTour,
 	updateTour,
 	deleteTour,
+	getTourStats
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkID);
 
+router.route('/tour-stats').get(getTourStats)
 router.route('/top-5-tours').get(aliasTopTours, getAllTours)
 router.route('/').get(getAllTours).post(createTour);
+
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
