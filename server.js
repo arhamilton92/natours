2d2d7b6d0b607708e0a9653db8ef03ae63a05e3d
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-04 19:16:49 -0400

    added: unhandled rejection error handler

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './.env' }); 		// ENV variables BEFORE EXPRESS
-const app = require('./app'); 			// EXPRESS
+dotenv.config({ path: './.env' }); // ENV variables BEFORE EXPRESS
+const app = require('./app'); // EXPRESS
 
 // DATABASE
 const password = process.env.DATABASE_PASSWORD;
@@ -21,6 +21,13 @@ mongoose
 
 // SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`App running on port ${port}`);
 }); // --------------------------------
+
+process.on('unhandledRejection', (err) => {
+	console.log(err.name, err.message);
+	server.close(() => {
+		process.exit(1)
+	})
+});
