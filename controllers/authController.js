59104d2bcfe98c81e70852ac64940c677e87e4fa
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-05 14:23:56 -0400

    added: signToken //implemented into userAuth functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,13 +5,17 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const appError = require('../utils/appError');
 
+const signToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
+}
+
 exports.signup = catchAsync(async (req, res, next) => {
 	const newUser = await User.create(
 		({ name, email, password, passwordConfirm } = req.body)
 	);
-	const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-		expiresIn: process.env.JWT_EXPIRES_IN,
-	});
+	const token = signToken(newUser._id)
 	res.status(201).json({
 		status: 'success',
 		data: {
@@ -32,10 +36,8 @@ exports.login = catchAsync(async (req, res, next) => {
 	if (!user || !user.correctPassword(password, user.password)) {
 		return next(new appError('Incorrect email or password', 401));
 	}
+	const token = signToken(user._id)
 	//
-	const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-		expiresIn: process.env.JWT_EXPIRES_IN,
-	});
 	res.status(200).json({
 		status: 'success',
 		token
