7d01aa82eb57af8010c9d205d08bfeb4cd18cbac
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-05 12:43:00 -0400

    updated: signup // jwt token sent

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 /** @format */
 
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -7,10 +8,15 @@ exports.signup = catchAsync(async (req, res, next) => {
 	const newUser = await User.create(
 		({ name, email, password, passwordConfirm } = req.body)
 	);
+	const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
+	//
 	res.status(201).json({
 		status: 'success',
 		data: {
-			user: newUser,
+            user: newUser,
+            token
 		},
 	});
 });
