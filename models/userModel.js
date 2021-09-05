59078b07fdfbf833dd1261273a74c91224d905d0
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-05 12:14:06 -0400

    added: .pre(save) bcrypt // document middleware to hash password and delete passwordConfirm

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // SCHEMA
 const userSchema = new mongoose.Schema({
@@ -13,9 +14,9 @@ const userSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
-        unique: true,
-        lowercase: true,
-        validate: [validator.isEmail, 'Please enter a valid email']
+		unique: true,
+		lowercase: true,
+		validate: [validator.isEmail, 'Please enter a valid email'],
 	},
 	photo: String,
 	password: {
@@ -23,16 +24,25 @@ const userSchema = new mongoose.Schema({
 		required: [true, 'Please enter a password'],
 		minLength: [8, 'A password must have minimum 8 characters'],
 	},
-    passwordConfirm: {
-        type: String,
+	passwordConfirm: {
+		type: String,
 		required: [true, 'Both password fields must match'],
 		validate: {
 			validator: function (el) {
-				return el === this.password
+				return el === this.password;
 			},
-			message: 'Passwords are not the same'
-		}
-    }
+			message: 'Passwords are not the same',
+		},
+	},
+}); // --------------------------------
+
+// DOCUMENT MIDDLEWARE
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) return next();
+	//
+	this.password = await bcrypt.hash(this.password, 12);
+	this.passwordConfirm = undefined;
+	next()
 }); // --------------------------------
 
 // MODEL
