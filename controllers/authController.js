c06764ca756879d00a4a780079c18e280aaa4338
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-05 12:32:16 -0400

    reformatting

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,9 +4,9 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-	const { name, email, password, passwordConfirm } = req.body;
-	const newUser = await User.create({ name, email, password, passwordConfirm });
-    //
+	const newUser = await User.create(
+		({ name, email, password, passwordConfirm } = req.body)
+	);
 	res.status(201).json({
 		status: 'success',
 		data: {
