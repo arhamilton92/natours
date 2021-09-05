9eafb8bc53e33df91daf3ea1fbb99580d1a558f4
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-05 14:08:36 -0400

    getAllUsers // working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,18 @@
+const User = require('../models/userModel');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync')
 
-exports.getAllUsers = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'route not yet implemented',
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+	const features = new APIFeatures(User.find(), req.query)
+	const users = await features.query;
+	//
+	res.status(200).json({
+		status: 'success',
+		results: users.length,
+		data: users,
 	});
-};
+});
+
 
 exports.getUser = (req, res) => {
 	res.status(500).json({
