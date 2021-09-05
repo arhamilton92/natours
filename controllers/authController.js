9ca8372c49bb9fb39dc051d9964f3f4e80d65a09
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-05 16:29:55 -0400

    reformatting

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,12 +29,11 @@ exports.signup = catchAsync(async (req, res, next) => {
 
 exports.login = catchAsync(async (req, res, next) => {
 	const { email, password } = req.body;
-	//
 	if (!email || !password) {
 		return next(new AppError('Please provide email and password', 400));
 	}
-	const user = await User.findOne({ email }).select('+password');
 	//
+	const user = await User.findOne({ email }).select('+password');
 	if (!user || !(await user.correctPassword(password, user.password))) {
 		return next(new AppError('Incorrect email or password', 401));
 	}
