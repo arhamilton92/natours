8315fc137da833d96611e6d64149d7b9d4533c73
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-05 12:29:32 -0400

    updated: signup // specified incoming fields to prevent unwanted values from being sent

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,9 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-	const newUser = await User.create(req.body);
-
+	const { name, email, password, passwordConfirm } = req.body;
+	const newUser = await User.create({ name, email, password, passwordConfirm });
+    //
 	res.status(201).json({
 		status: 'success',
 		data: {
