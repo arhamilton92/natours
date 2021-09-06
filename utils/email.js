eb3d9df98bd27326b74880696dc2f5039094f7a3
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-06 15:05:41 -0400

    created: email.js // email clients

---
+++ utils/email.js
@@ -0,0 +1,26 @@
+/** @format */
+
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+	const { EMAIL_HOST, EMAIL_PORT, EMAIL_USERNAME, EMAIL_PASSWORD } =
+		process.env;
+	const transporter = nodemailer.createTransport({
+		host: EMAIL_HOST,
+		port: EMAIL_PORT,
+		auth: {
+			user: EMAIL_USERNAME,
+			pass: EMAIL_PASSWORD,
+		},
+	});
+	const mailOptions = {
+		from: 'Andrea Hamilton <arham@email.com>',
+		to: options.email,
+		subject: options.subject,
+		text: options.text,
+	};
+	//
+	await transporter.sendMail(mailOptions);
+};
+
+modules.exports = sendEmail;
