375f42e1fdc105dbbdc46bdab978fae9adba0ca8
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-08 15:10:40 -0400

    added: rateLimit // middleware limits the amount of requests per IP per hour

--- app.js
+++ app.js
@@ -2,19 +2,26 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 //
-const AppError = require('./utils/AppError')
-const globalErrorHandler = require('./controllers/errorController')
+const AppError = require('./utils/AppError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARE
+// GLOBAL MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
 	console.log('development');
 	app.use(morgan('dev'));
 }
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 // ------------------------------------
@@ -23,8 +30,9 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 //
-app.all('*', (req, res, next) => { 			// incorrect URL error
-	next(new AppError(`Can't find ${req.originalUrl}`, 404))
+app.all('*', (req, res, next) => {
+	// incorrect URL error
+	next(new AppError(`Can't find ${req.originalUrl}`, 404));
 });
 app.use(globalErrorHandler);
 // --------------------------------
