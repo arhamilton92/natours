1b0feb5cea80abef21d25c869d6793f108c49da7
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-08 12:08:17 -0400

    userSchema.pre('save') // checks if password has been changed, then updated passwordChangedAt property

--- models/userModel.js
+++ models/userModel.js
@@ -53,6 +53,12 @@ userSchema.pre('save', async function (next) {
 	this.password = await bcrypt.hash(this.password, 12);
 	this.passwordConfirm = undefined;
 	next();
+});
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password') || this.isNew) return next();
+	//
+	this.passwordChangedAt = Date.now() - 1000;
+	next()
 }); // --------------------------------
 
 // INSTANCE METHOD
