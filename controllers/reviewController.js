c03fa29b320ff514e2356de658808e1d1015b45c
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-11 20:50:49 -0400

    created: reviewController.js // added functions to find all and find one review

---
+++ controllers/reviewController.js
@@ -0,0 +1,32 @@
+/** @format */
+
+const Review = require('../models/reviewModel');
+const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/AppError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+	const features = new APIFeatures(Review.find(), req.query)
+		.filter()
+		.sort()
+		.limitFields()
+		.paginate();
+	const reviews = await features.query;
+	//
+	res.status(200).json({
+		status: 'success',
+		results: reviews.length,
+		data: reviews,
+	});
+}); // --------------------------------
+
+exports.getReview = catchAsync(async (req, res, next) => {
+	const review = await Review.findById(req.params.id).populate().exec();
+	if (!review) {
+		return next(new AppError('No review found with that ID', 404));
+	}
+	res.status(200).json({
+		status: 'success',
+		data: review,
+	});
+}); // --------------------------------
