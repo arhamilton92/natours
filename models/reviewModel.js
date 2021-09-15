6fa364e2886daa2cb771339e0c94a268d83da0eb
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-15 17:20:28 -0400

    adding: statics.caulculateAverageRatings // gets average rating of all a tours reviews when a new review is added and updates tour info to reflect new average and total.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
 	review: String,
@@ -27,6 +28,9 @@ const reviewSchema = new mongoose.Schema({
 }); // --------------------------------
 
 // DOCUMENT MIDDLEWARE
+reviewSchema.post('save', function (next) {
+	this.constructor.calculateAverageRatings(this.tour);
+}); // --------------------------------
 // ------------------------------------
 
 // QUERY MIDDLEWARE
@@ -34,12 +38,34 @@ reviewSchema.pre(/^find/, function (next) {
 	this.populate({
 		path: 'user',
 		select: 'name photo',
-    })
+	});
 	next();
 }); // --------------------------------
 // ------------------------------------
 
 // INSTANCE METHOD
+// ------------------------------------
+
+// STATIC METHOD
+reviewSchema.statics.calculateAverageRatings = async function (tourId) {
+	const stats = await this.aggregate([
+		{
+			$match: { tour: tourId },
+		},
+		{
+			$group: {
+				_id: '$tour',
+				nRating: { $sum: 1 },
+				avgRating: { $avg: '$rating' },
+			},
+		},
+	]);
+	console.log(stats);
+	await Tour.findByIdAndUpdate(tourId, {
+		ratingsQuantity: stats[0].nRating,
+		ratingsAverage: stats[0].avgRating,
+	});
+}; // ---------------------------------
 // ------------------------------------
 
 const Review = mongoose.model('Review', reviewSchema);
