a9726388018c6dbd510be7ec59aa4574389b4927
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-18 13:47:53 -0400

    updated: getOverview // gets and send all tours data

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,13 +1,23 @@
 /** @format */
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res) => {
+	// get tours data
+    const tours = await Tour.find();
+
+	// build template
+
+	//render template with data
+
 	res.status(200).render('overview', {
-		title: 'All Tours',
+        title: 'All Tours',
+        tours
 	});
-};
+});
 
 exports.getTour = (req, res) => {
 	res.status(200).render('tour', {
-		title: 'The Forest Hiker'
+		title: 'The Forest Hiker',
 	});
 };
