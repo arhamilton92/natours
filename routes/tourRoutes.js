c59446c0df4c15b615af6f62a541b4a6a9118599
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-17 20:39:11 -0400

    added: .getDistances // finds tours by distance from given coordinates, then lists distance in order of closest to that point. implemented into routes.

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,6 +12,7 @@ const {
 	getTourStats,
 	getMonthlyPlan,
 	getToursWithin,
+	getDistances
 } = require('../controllers/tourController');
 const reviewRouter = require('./reviewRoutes.js');
 
@@ -21,9 +22,8 @@ router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 
-router
-	.route('/within/:distance/center/:latlng/unit/:unit')
-	.get(getToursWithin);
+router.route('/distances/:latlng/unit/:unit').get(getDistances);
+router.route('/within/:distance/center/:latlng/unit/:unit').get(getToursWithin);
 
 router
 	.route('/')
