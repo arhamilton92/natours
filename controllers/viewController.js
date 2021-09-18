c39d397bff185db6dd7f5ceaa486d7d0154b4c4e
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-18 13:40:36 -0400

    refactoring: viewRoutes // created: viewController.js // moved route functions to viewController and included them in viewRoutes

---
+++ controllers/viewController.js
@@ -0,0 +1,13 @@
+/** @format */
+
+exports.getOverview = (req, res) => {
+	res.status(200).render('overview', {
+		title: 'All Tours',
+	});
+};
+
+exports.getTour = (req, res) => {
+	res.status(200).render('tour', {
+		title: 'The Forest Hiker'
+	});
+};
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,24 +1,9 @@
 const express = require('express');
+const { getOverview, getTour } = require('../controllers/viewController')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.status(200).render('base', {
-		tour: 'The Forest Hiker',
-		user: 'andrea'
-	});
-});
-
-router.get('/overview', (req, res) => {
-	res.status(200).render('overview', {
-		title: 'All Tours'
-	});
-})
-
-router.get('/tour', (req, res) => {
-	res.status(200).render('tour', {
-		title: 'The Forest Hiker'
-	});
-})
+router.get('/', getOverview)
+router.get('/tour', getTour)
 
 module.exports = router;
