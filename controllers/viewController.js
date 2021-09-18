eff0161dba3ea4444e0e11e8f356cff3b2821292
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-18 17:36:26 -0400

    getTour gets tour by slug

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,20 +4,25 @@ const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
 	// get tours data
-    const tours = await Tour.find();
+	const tours = await Tour.find();
 
 	// build template
 
 	//render template with data
 
 	res.status(200).render('overview', {
-        title: 'All Tours',
-        tours
+		title: 'All Tours',
+		tours,
 	});
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync( async (req, res) => {
+	const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+		path: 'reviews',
+		fields: 'review rating user',
+	});
 	res.status(200).render('tour', {
 		title: 'The Forest Hiker',
+		tour,
 	});
-};
+});
