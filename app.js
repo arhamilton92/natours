987fa81207f9e4cb85efef7089425fdd525b5ccb
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-18 12:23:03 -0400

    formatting for my sanity

--- app.js
+++ app.js
@@ -1,42 +1,60 @@
 /** @format */
 
+const path = require('path');
 const express = require('express');
+// DEV
 const morgan = require('morgan');
+// SECURITY
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-//
+// UTILS
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes');
+const reviewRouter = require('./routes/reviewRoutes')
+// ---------------------------
 
 const app = express();
 
-// GLOBAL MIDDLEWARE
-app.use(helmet()); // SECURITY HTTP HEADERS
+// SET UP PUG TEMPLATE ENGINE
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+
+// ---------------------------
+// GLOBAL MIDDLEWARE 
+// ---------------------------
+
+// ENV SETUP
 if (process.env.NODE_ENV === 'development') {
+	// DEV LOGGING
 	console.log('development');
 	app.use(morgan('dev')); // DEV LOGGING
-} // ----------------------------------
+}
+// SET SECURITY HTTP HEADERS
+app.use(helmet()); 
+
+// LIMIT IP REQUEST RATE
 const limiter = rateLimit({
 	max: 2000,
 	windowMs: 60 * 60 * 1000,
 	message: 'Too many requests from this IP, please try again in an hour',
-}); // --------------------------------
-app.use('/api', limiter); // LIMIT IP REQ RATE
-// ------------------------------------
-app.use(express.json({ limit: '10kb' })); // PARSE BODY & LIMIT TO 10kb SIZE
-// ------------------------------------
-app.use(mongoSanitize()); // DATA SANITIZATION AGAINST noSQL QUERY INJECTION
-// ------------------------------------
-app.use(xss()); // DATA SANITIZATION AGAINST XSS
-// ------------------------------------
+});
+app.use('/api', limiter);
+
+// PARSE & LIMIT REQ BODY SIZE
+app.use(express.json({ limit: '10kb' }));
+
+// DATA SANITIZATION
+app.use(mongoSanitize()); // noSQL query injection
+app.use(xss()); // XSS
+
+// PREVENT PARAM POLLUTION
 app.use(
-	// PREVENT PARAM POLLUTION
 	hpp({
 		whitelist: [
 			'duration',
@@ -47,20 +65,22 @@ app.use(
 			'maxGroupSize',
 		],
 	})
-); // ---------------------------------
-app.use(express.static(`${__dirname}/public`)); // SERVE STATIC FILES
-// ------------------------------------
+);
 
+app.use(express.static(path.join(__dirname, 'views'))); // SERVE STATIC FILES
+
+// --------------------------------
 // ROUTER
+// --------------------------------
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/review', reviewRouter);
-// ------------------------------------
+//
 app.all('*', (req, res, next) => {
 	// incorrect URL error
 	next(new AppError(`Can't find ${req.originalUrl}`, 404));
 });
 app.use(globalErrorHandler);
-// ------------------------------------
 
 module.exports = app;
