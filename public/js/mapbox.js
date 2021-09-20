b56ce4eae4e8b6bb8bb8c30f0655d6a1a7e6c2bc
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-20 17:35:29 -0400

    mapbox // custom style. changed default location setting

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -8,5 +8,7 @@ mapboxgl.accessToken =
 	'pk.eyJ1IjoiYXJoYW1pbHRvbiIsImEiOiJja3R0MDZhejYxa3R0MnZvMmdsZ3lseWc5In0.ASj5wPRzqBaDCzed8QtKZA';
 var map = new mapboxgl.Map({
 	container: 'map',
-	style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/arhamilton/cktt0zgo70jg518pe090z88a8',
+    center: [-118.113491, 34.111745],
+    zoom: 5
 });
