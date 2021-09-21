bf43d910cbee0f594af7c1911bacdf98051e17f1
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-21 15:33:34 -0400

    imported JS functions into parcel bundler. added alerts when logging in

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,8 @@
-/** @format */
-
 /* eslint-disable */
-console.log('login JS file connected');
+import axios from 'axios';
+import { showAlert } from './alerts'
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
 	try {
 		const res = await axios({
 			method: 'POST',
@@ -14,18 +13,12 @@ const login = async (email, password) => {
 			},
 		});
 		if (res.data.status === 'success') {
+			showAlert('success', 'Logged in successfully!')
 			window.setTimeout(() => {
 				location.assign('/');
 			}, 100);
 		}
 	} catch (error) {
-		alert(error.response.data.message);
+		showAlert('error', error.response.data.message)
 	}
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-	e.preventDefault();
-	const email = document.getElementById('email').value;
-	const password = document.getElementById('password').value;
-	login(email, password);
-});
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,47 +1,46 @@
 /** @format */
 
 /* eslint-disable */
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+export const displayMap = (locations) => {
+	mapboxgl.accessToken =
+		'pk.eyJ1IjoiYXJoYW1pbHRvbiIsImEiOiJja3R0MDZhejYxa3R0MnZvMmdsZ3lseWc5In0.ASj5wPRzqBaDCzed8QtKZA';
+	var map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/arhamilton/cktt0zgo70jg518pe090z88a8',
+		scrollZoom: false,
+	});
 
-mapboxgl.accessToken =
-	'pk.eyJ1IjoiYXJoYW1pbHRvbiIsImEiOiJja3R0MDZhejYxa3R0MnZvMmdsZ3lseWc5In0.ASj5wPRzqBaDCzed8QtKZA';
-var map = new mapboxgl.Map({
-	container: 'map',
-    style: 'mapbox://styles/arhamilton/cktt0zgo70jg518pe090z88a8',
-    scrollZoom: false
-});
+	const bounds = new mapboxgl.LngLatBounds();
 
-const bounds = new mapboxgl.LngLatBounds();
+	locations.forEach((loc) => {
+		// create location marker
+		const el = document.createElement('div');
+		el.className = 'marker';
+		//
+		// add marker
+		new mapboxgl.Marker({
+			element: el,
+			anchor: 'bottom',
+		})
+			.setLngLat(loc.coordinates)
+			.addTo(map);
+		//
+		// add popup
+		new mapboxgl.Popup({ offset: 30 })
+			.setLngLat(loc.coordinates)
+			.setHTML(`<p>Day${loc.day}: ${loc.description}</p>`)
+			.addTo(map);
+		//
+		// extend map bounds to include current location
+		bounds.extend(loc.coordinates);
+	});
 
-locations.forEach((loc) => {
-	// create location marker
-	const el = document.createElement('div');
-	el.className = 'marker';
-    //
-	// add marker
-	new mapboxgl.Marker({
-		element: el,
-		anchor: 'bottom',
-	})
-		.setLngLat(loc.coordinates)
-		.addTo(map);
-    //
-	// add popup
-	new mapboxgl.Popup({ offset: 30 })
-		.setLngLat(loc.coordinates)
-		.setHTML(`<p>Day${loc.day}: ${loc.description}</p>`)
-		.addTo(map);
-    //
-	// extend map bounds to include current location
-	bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-	padding: {
-		top: 200,
-		bottom: 200,
-		left: 50,
-		right: 50,
-	},
-});
+	map.fitBounds(bounds, {
+		padding: {
+			top: 200,
+			bottom: 200,
+			left: 50,
+			right: 50,
+		},
+	});
+};
