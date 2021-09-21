3c8f0d5ac2be22b7c5e04f8d2155407717b926bf
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-21 13:44:39 -0400

    alerts error message if login unsuccessful. if successful, redirects to /

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,31 @@
 /** @format */
 
 /* eslint-disable */
-console.log('login JS file connected')
+console.log('login JS file connected');
 
 const login = async (email, password) => {
-    try {
-        const res = await axios({
-            method: 'POST',
-            url: 'http://localhost:8000/api/v1/users/login',
-            data: {
-                email,
-                password
-            }
-        });
-        console.log(res)
-    } catch (error) {
-        console.log(error)
-    }
-}
+	try {
+		const res = await axios({
+			method: 'POST',
+			url: 'http://localhost:8000/api/v1/users/login',
+			data: {
+				email,
+				password,
+			},
+		});
+		if (res.data.status === 'success') {
+			window.setTimeout(() => {
+				location.assign('/');
+			}, 100);
+		}
+	} catch (error) {
+		alert(error.response.data.message);
+	}
+};
 
 document.querySelector('.form').addEventListener('submit', (e) => {
 	e.preventDefault();
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    login(email, password)
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	login(email, password);
 });
