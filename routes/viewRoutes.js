8b12a9ced5c6d6df07b23015dcbcfaee68e538ab
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-21 17:26:17 -0400

    added: routes for updating user

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getOverview, getTour, getLoginForm, getAccount } = require('../controllers/viewController')
+const { getOverview, getTour, getLoginForm, getAccount, updateUserData } = require('../controllers/viewController')
 const { protect, isLoggedIn} = require('../controllers/authController')
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/', isLoggedIn, getOverview)
 router.get('/tour/:slug', isLoggedIn, getTour)
 router.get('/login', isLoggedIn, getLoginForm)
 router.get('/me', protect, getAccount)
+router.post('/submit-user-data', protect, updateUserData)
 
 module.exports = router;
