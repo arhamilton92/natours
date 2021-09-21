973f856cd242e293f310f5fb1da8bb92ff1c1b37
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-21 12:05:11 -0400

    added popup above location markers showing day and location name

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -8,31 +8,39 @@ mapboxgl.accessToken =
 	'pk.eyJ1IjoiYXJoYW1pbHRvbiIsImEiOiJja3R0MDZhejYxa3R0MnZvMmdsZ3lseWc5In0.ASj5wPRzqBaDCzed8QtKZA';
 var map = new mapboxgl.Map({
 	container: 'map',
-    style: 'mapbox://styles/arhamilton/cktt0zgo70jg518pe090z88a8',
+	style: 'mapbox://styles/arhamilton/cktt0zgo70jg518pe090z88a8',
 });
 
 const bounds = new mapboxgl.LngLatBounds();
 
-locations.forEach(loc => {
-    // create location marker
-    const el = document.createElement('div');
-    el.className = 'marker'
-    
-    // add marker
-    new mapboxgl.Marker({
-        element: el,
-        anchor: 'bottom'
-    }).setLngLat(loc.coordinates).addTo(map)
-    
-    // extend map bounds to include current location
-    bounds.extend(loc.coordinates)
-})
+locations.forEach((loc) => {
+	// create location marker
+	const el = document.createElement('div');
+	el.className = 'marker';
+
+	// add marker
+	new mapboxgl.Marker({
+		element: el,
+		anchor: 'bottom',
+	})
+		.setLngLat(loc.coordinates)
+		.addTo(map);
+
+	//add popup
+	new mapboxgl.Popup({ offset: 30 })
+		.setLngLat(loc.coordinates)
+		.setHTML(`<p>Day${loc.day}: ${loc.description}</p>`)
+		.addTo(map);
+
+	// extend map bounds to include current location
+	bounds.extend(loc.coordinates);
+});
 
 map.fitBounds(bounds, {
-    padding: {
-        top: 200,
-        bottom: 200,
-        left: 100,
-        right: 100
-    }
-})
+	padding: {
+		top: 200,
+		bottom: 200,
+		left: 100,
+		right: 100,
+	},
+});
