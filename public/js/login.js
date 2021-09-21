0b226c464a55d4d12e69288866635f377ccd40b1
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-21 13:06:13 -0400

    working login function returns JWT

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+/** @format */
+
+/* eslint-disable */
+console.log('login JS file connected')
+
+const login = async (email, password) => {
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: 'http://localhost:8000/api/v1/users/login',
+            data: {
+                email,
+                password
+            }
+        });
+        console.log(res)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+document.querySelector('.form').addEventListener('submit', (e) => {
+	e.preventDefault();
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    login(email, password)
+});
