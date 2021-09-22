7413807d00274c61041b5f3adb63182cf3f4970e
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-22 19:40:08 -0400

    updated: resizeTourImages // uploads iages properly

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -36,10 +36,32 @@ exports.uploadTourImages = upload.fields([
 	{ name: 'images', maxCount: 3 },
 ]);
 
-exports.resizeTourImages = (req, res, next) => {
-	console.log(req.files);
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+	if (!req.files.imageCover || !req.files.images) return next();
+	req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+	//imageCover
+	await sharp(req.files.imageCover[0].buffer)
+		.resize(2000, 1333)
+		.toFormat('jpeg')
+		.jpeg({ quality: 90 })
+		.toFile(`public/img/tours/${req.body.imageCover}`);
+	// images array
+	req.body.images = [];
+	await Promise.all(
+		req.files.images.map(async (file, i) => {
+			const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+			await sharp(req.files.images[i].buffer)
+				.resize(2000, 1333)
+				.toFormat('jpeg')
+				.jpeg({ quality: 90 })
+				.toFile(`public/img/tours/${filename}`);
+
+			req.body.images.push(filename);
+		})
+	);
+
 	next();
-};
+});
 
 exports.getAllTours = factory.getAll(Tour);
 
