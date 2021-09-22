d0196fa2d806d3bc0e7fb23dc66341e15326a417
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-22 16:28:21 -0400

    added: sharp configuration. commented out old multer code, using sharp isntead

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,15 +9,16 @@ const multer = require('multer'); // file uploads
 const sharp = require('sharp'); // image processing
 
 // MULTER SETUP
-const multerStorage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'public/img/users');
-	},
-	filename: (req, file, cb) => {
-		const ext = file.mimetype.split('/')[1];
-		cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-	},
-});
+// const multerStorage = multer.diskStorage({
+// 	destination: (req, file, cb) => {
+// 		cb(null, 'public/img/users');
+// 	},
+// 	filename: (req, file, cb) => {
+// 		const ext = file.mimetype.split('/')[1];
+// 		cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+// 	},
+// });
+const multerStorage = multer.memoryStorage();
 const multerFilter = (req, file, cb) => {
 	if (file.mimetype.startsWith('image')) {
 		cb(null, true);
@@ -41,6 +42,13 @@ exports.getMe = (req, res, next) => {
 exports.uploadUserPhoto = upload.single('photo');
 exports.resizeUserPhoto = (req, res, next) => {
 	if (!req.file) return next();
+	req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+	sharp(req.file.buffer)
+		.resize(500, 500)
+		.toFormat('jpeg')
+		.jpeg({ quality: 90 })
+		.toFile(`public/img/users/${req.file.filename}`);
+	next();
 };
 // -----------------------------------------
 // ^ MIDDLEWARE ^ --------------------------
