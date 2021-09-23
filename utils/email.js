91deda2f867f4793ad5b591058705fa89a1470ac
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-23 13:08:34 -0400

    updated // removed old emailing code, continued building new email constructor

--- utils/email.js
+++ utils/email.js
@@ -2,6 +2,7 @@
 
 const nodemailer = require('nodemailer');
 const pug = require('pug')
+const htmlToText = require('html-to-text')
 
 const {
 	EMAIL_FROM,
@@ -19,13 +20,11 @@ module.exports = class Email {
 		this.url = url;
 		this.from = `Andrea Hamilton <${EMAIL_FROM}>`;
 	}
-
-	createTransport() {
+	transport() {
 		if (NODE_ENV === 'production') {
 			// sendgrid
 			return 1;
 		}
-
 		return nodemailer.createTransport({
 			host: EMAIL_HOST,
 			port: EMAIL_PORT,
@@ -35,42 +34,24 @@ module.exports = class Email {
 			},
 		});
 	}
-
-	send(template, subject) {
-		// render HTML with pug
-		const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`)
-		
-		// define email options
+	async send(template, subject) {
+		const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+			firstName: this.firstName,
+			url: this.url,
+			subject
+		})
 		const mailOptions = {
-			from: `Andrea Hamilton ${EMAIL_FROM}`,
-			to: options.email,
-			subject: options.subject,
-			text: options.text,
-			html
+			from: this.from,
+			to: this.to,
+			subject,
+			html,
+			text: htmlToText.fromString(html)
 		};
-
+		//
+		await this.transport().sendMail(mailOptions)
+		//
+	}
 	sendWelcome() {
 		this.send('welcome', 'Welcome to Natours!')
 	}
 };
-
-const sendEmail = async (options) => {
-	const transporter = nodemailer.createTransport({
-		host: EMAIL_HOST,
-		port: EMAIL_PORT,
-		auth: {
-			user: EMAIL_USERNAME,
-			pass: EMAIL_PASSWORD,
-		},
-	});
-	const mailOptions = {
-		from: 'Andrea Hamilton <arham@email.com>',
-		to: options.email,
-		subject: options.subject,
-		text: options.text,
-	};
-	//
-	await transporter.sendMail(mailOptions);
-}; // ---------------------------------
-
-module.exports = sendEmail;
