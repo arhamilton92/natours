46de5a7a255994a03e5f675e02664d621af52c5c
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-24 14:41:54 -0400

    working stripe test payments

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -3222,20 +3222,21 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 /** @format */
 const bookTour = async tourId => {
   // get checkout session from API
+  const stripe = Stripe('pk_test_51Jcx3mI20R6TlRgOtSizmRe5rWEEmFO56L1C4DBoC2r99xruz5vKoHpZPQS4H0FbbUy7xNsDw4GK3WnKGgPymSn600ocF1wbte');
+
   try {
     const session = await (0, _axios.default)({
       method: 'GET',
       url: `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`
     });
-
-    if (session.data.status === 'success') {
-      console.log(session);
-    }
+    console.log(session);
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
   } catch (error) {
+    console.log('error');
     (0, _alerts.showAlert)('error', error.response.data.message);
-  } //
-  // create checkout form + charge credit card
-
+  }
 };
 
 exports.bookTour = bookTour;
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -4,17 +4,18 @@ import { showAlert } from './alerts';
 
 export const bookTour = async (tourId) => {
     // get checkout session from API
+    const stripe = Stripe('pk_test_51Jcx3mI20R6TlRgOtSizmRe5rWEEmFO56L1C4DBoC2r99xruz5vKoHpZPQS4H0FbbUy7xNsDw4GK3WnKGgPymSn600ocF1wbte');
     try {
 		const session = await axios({
 			method: 'GET',
 			url: `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`,
-		});
-		if (session.data.status === 'success') {
-			console.log(session)
-		}
+        });
+        console.log(session)
+        await stripe.redirectToCheckout({
+            sessionId: session.data.session.id
+        })
     } catch (error) {
+        console.log('error')
         showAlert('error', error.response.data.message);
     }
-    //
-    // create checkout form + charge credit card
 };
