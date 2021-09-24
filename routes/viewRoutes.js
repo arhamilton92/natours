992a21c87112a1b9a46b5550ea2c06037f69ad08
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-24 15:20:22 -0400

    create booking from query string // TEMPORARY/INSECURE

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getOverview, getTour, getLoginForm, getAccount, updateUserData } = require('../controllers/viewController')
-const { protect, isLoggedIn} = require('../controllers/authController')
+const { protect, isLoggedIn } = require('../controllers/authController')
+const { createBookingCheckout } = require('../controllers/bookingController')
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, getOverview)
+router.get('/', createBookingCheckout, isLoggedIn, getOverview)
 router.get('/tour/:slug', isLoggedIn, getTour)
 router.get('/login', isLoggedIn, getLoginForm)
 router.get('/me', protect, getAccount)
