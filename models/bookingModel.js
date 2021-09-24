3185c2ed26778bc7e83084f0a6fd1a7a96b210cd
Andrea RH <andrea.r.hamilton92@gmail.com>
2021-09-24 14:56:14 -0400

    added: pre find populate

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -27,5 +27,12 @@ const bookingSchema = new mongoose.Schema({
 	},
 });
 
+bookingSchema.pre(/^find/, function (next) {
+    this.populate('user').populate({
+        path: 'tour',
+        select: 'name'
+    })
+})
+
 const Booking = mongoose.model('Booking', bookingSchema);
 module.exports = Booking;
