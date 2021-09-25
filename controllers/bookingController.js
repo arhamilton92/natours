2036068e9c622ab9d21ecbfc13a3022041e892d9
Andrea Hamilton <andrea.r.hamilton92@gmail.com>
2021-09-25 18:55:09 -0400

    stripe troubleshooting

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -33,20 +33,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 		client_reference_id: req.params.tourId,
 		line_items: [
 			{
+				name: `${tour.name} Tour`,
+				description: tour.summary,
+				images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+				amount: tour.price * 100,
+				currency: 'usd',
 				quantity: 1,
-				price_data: {
-					currency: 'usd',
-					unit_amount: tour.price * 100,
-					product_data: {
-						name: `${tour.name} Tour`,
-						description: tour.summary,
-						images: [
-							`${req.protocol}://${req.get('host')}/img/tours/${
-								tour.imageCover
-							}`,
-						],
-					},
-				},
 			},
 		],
 	});
@@ -66,7 +58,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-	console.log('----------CREATING BOOKING-------------')
+	console.log('-------------create booking-------------')
 	const tour = session.client_reference_id;
 	const user = (await User.findOne({ email: session.customer_email })).id;
 	const price = session.amount_total / 100;
@@ -86,8 +78,9 @@ exports.webhookCheckout = async (req, res, next) => {
 			signature,
 			process.env.STRIPE_WEBHOOK_SECRET
 		);
-		console.log(event);
+		console.log(event)
 		if (event.type === 'checkout.session.completed') {
+			console.log('event success')
 			createBookingCheckout(event.data.object);
 		}
 		res.status(200).json({ recieved: true });
